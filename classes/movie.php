56dc3b15f36cd638203153c2036a5c65dab631fb
AndreaDeCeglia <deceglia89@gmail.com>
2022-12-16 15:52:53 +0100

    exercise done

--- classes/movie.php
+++ classes/movie.php
@@ -2,14 +2,26 @@
 
 class Movie{
 
-    $title;
-    $year;
-    $genre;
+    public $title;
+    public $year;
+    public $genre;
+    public $vote; 
 
-    function __construct( $_x, $_y, $_z ){
+    function __construct( $_x, $_y, $_z){
         $this->title = $_x;
         $this->year = $_y;
         $this->genre = $_z;
+        $this->vote =  rand(1, 10);
+    }
+
+    public function isMust(){
+        if( $this->vote < 6){
+            return "$this->title is not adviced!!";
+        } elseif( $this->vote > 5 && $this->vote < 8 ) {
+            return "$this->title is adviced";
+        } else {
+            return "$this->title is a Must .... wacth it now!!!";
+        }
     }
 }
 
--- index.php
+++ index.php
@@ -3,3 +3,46 @@
 include_once __DIR__ . './classes/movie.php';
 
 ?>
+
+<!-- html's form -->
+
+<h1>
+    insert your fovourite movie !!!
+</h1>
+
+<form method="GET">
+    <input type="text" name="title" placeholder="write the title">
+    <input type="number" name="year" placeholder="movie's year">
+    <input type="text" name="genre" placeholder="write the genre">
+    <button>send</button>
+</form>
+
+<?php 
+
+if( !empty($_GET) && !empty( $_GET['title']) && !empty( $_GET['year']) && !empty( $_GET['genre'])){
+    $title = $_GET['title'];
+    $year = $_GET['year'];
+    $genre = $_GET['genre'];
+
+    $Movie = new Movie( $title, $year, $genre);
+    var_dump( $Movie );
+}
+
+$istancesArray = [];
+
+echo $Movie-> isMust();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
